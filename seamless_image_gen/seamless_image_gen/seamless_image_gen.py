baa29e65c29beefd57ffeb20c68f2953eb095d28
xygao <xygao0703@gmail.com>
2021-05-24 21:37:08 +0900

    add cv2 image base class

--- seamless_image_gen/seamless_image_gen/seamless_image_gen.py
+++ seamless_image_gen/seamless_image_gen/seamless_image_gen.py
@@ -1,16 +1,48 @@
 from abc import ABCMeta, abstractmethod
 
 import cv2
+import numpy as np
 
 
 class ImageGenerator(metaclass=ABCMeta):
     @staticmethod
     @abstractmethod
-    def proc(input_path, output_path):
+    def read(input_path):
         pass
 
-
-class OriginalImageGenerator(ImageGenerator):
     @staticmethod
-    def proc(input_path, output_path):
-        cv2.imwrite(output_path, cv2.imread(input_path))
+    @abstractmethod
+    def write(output_path, image):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def proc(image):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def __call__(cls, input_path, output_path):
+        cls.write(output_path, cls.proc(cls.read(input_path)))
+
+
+class CV2IdenticalImageGenerator(ImageGenerator):
+    @staticmethod
+    def read(input_path: str) -> np.ndarray:
+        return cv2.imread(input_path)
+
+    @staticmethod
+    def write(output_path: str, image: np.ndarray) -> None:
+        cv2.imwrite(output_path, image)
+
+    @staticmethod
+    def proc(image: np.ndarray) -> np.ndarray:
+        return image
+
+    @classmethod
+    def __call__(cls, input_path, output_path):
+        return super().__call__(input_path, output_path)
+
+
+class CenterOfTileImageGenerator(CV2IdenticalImageGenerator):
+    pass
--- seamless_image_gen/tests/test_seamless_image_gen.py
+++ seamless_image_gen/tests/test_seamless_image_gen.py
@@ -1,6 +1,6 @@
 import os
 
-from seamless_image_gen import OriginalImageGenerator
+from seamless_image_gen import CenterOfTileImageGenerator, CV2IdenticalImageGenerator
 
 
 def file_exist(path):
@@ -11,12 +11,23 @@ def file_exist(path):
         return False
 
 
-def test_original_image_generator():
+def test_cv2_identical_image_generator():
     input_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "test_imgs/1.jpg"
     )
     output_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "test_imgs/out_1.jpg"
     )
-    OriginalImageGenerator.proc(input_path, output_path)
+    CV2IdenticalImageGenerator()(input_path, output_path)
+    assert file_exist(output_path)
+
+
+def test_center_of_tile_image_generator():
+    input_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "test_imgs/1.jpg"
+    )
+    output_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "test_imgs/out_1.jpg"
+    )
+    CenterOfTileImageGenerator()(input_path, output_path)
     assert file_exist(output_path)
