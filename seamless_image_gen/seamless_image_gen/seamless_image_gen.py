7b69b3dd5db13b9c6b6b276202ff61b32c9f640d
xygao <xygao0703@gmail.com>
2021-05-26 23:33:44 +0900

    add mask generator

--- seamless_image_gen/seamless_image_gen/seamless_image_gen.py
+++ seamless_image_gen/seamless_image_gen/seamless_image_gen.py
@@ -45,4 +45,30 @@ class CV2IdenticalImageGenerator(ImageGenerator):
 
 
 class CenterOfTileImageGenerator(CV2IdenticalImageGenerator):
-    pass
+    @staticmethod
+    def proc(image: np.ndarray) -> np.ndarray:
+        center_x = image.shape[1] / 2
+        center_y = image.shape[0] / 2
+
+        image_2x1 = np.concatenate((image, image), axis=0)
+        image_2x2 = np.concatenate((image_2x1, image_2x1), axis=1)
+        return image_2x2[
+            int(center_y) : int(center_y + image.shape[1]),
+            int(center_x) : int(center_x + image.shape[0]),
+        ]
+
+
+class CenterCrossMaskImageGenerator(CV2IdenticalImageGenerator):
+    @staticmethod
+    def proc(image: np.ndarray) -> np.ndarray:
+        mask = np.zeros(image.shape[:2], dtype="uint8")
+        center_x = mask.shape[1] / 2
+        center_y = mask.shape[0] / 2
+        w = mask.shape[1] * 0.1
+        h = mask.shape[0] * 0.1
+        x = center_x - w / 2
+        y = center_y - h / 2
+        mask[int(y) : int(y + h), :] = 1
+        mask[:, int(x) : int(x + w)] = 1
+        mask = cv2.cvtColor(mask * 255, cv2.COLOR_GRAY2RGB)
+        return mask
--- seamless_image_gen/tests/test_seamless_image_gen.py
+++ seamless_image_gen/tests/test_seamless_image_gen.py
@@ -1,33 +1,54 @@
+import filecmp
 import os
 
-from seamless_image_gen import CenterOfTileImageGenerator, CV2IdenticalImageGenerator
+import pytest
+from seamless_image_gen import (
+    CenterCrossMaskImageGenerator,
+    CenterOfTileImageGenerator,
+    CV2IdenticalImageGenerator,
+)
 
 
-def file_exist(path):
-    if os.path.exists(path):
-        os.remove(path)
-        return True
+def files_equal(acutal_path, expected_path):
+    if os.path.exists(acutal_path):
+        is_equal = filecmp.cmp(acutal_path, expected_path, shallow=False)
+        print(is_equal)
+        os.remove(acutal_path)
+        return is_equal
     else:
         return False
 
 
-def test_cv2_identical_image_generator():
-    input_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "test_imgs/1.jpg"
-    )
-    output_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "test_imgs/out_1.jpg"
-    )
-    CV2IdenticalImageGenerator()(input_path, output_path)
-    assert file_exist(output_path)
+def abs_path(file_path):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), file_path)
 
 
-def test_center_of_tile_image_generator():
-    input_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "test_imgs/1.jpg"
-    )
-    output_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "test_imgs/out_1.jpg"
-    )
-    CenterOfTileImageGenerator()(input_path, output_path)
-    assert file_exist(output_path)
+@pytest.mark.parametrize(
+    "image_generator, input_file, actual_file, expected_file",
+    [
+        (
+            CV2IdenticalImageGenerator(),
+            "test_imgs/original.png",
+            "test_imgs/actual_1.png",
+            "test_imgs/expected_1.png",
+        ),
+        (
+            CenterOfTileImageGenerator(),
+            "test_imgs/original.png",
+            "test_imgs/actual_2.png",
+            "test_imgs/expected_2.png",
+        ),
+        (
+            CenterCrossMaskImageGenerator(),
+            "test_imgs/original.png",
+            "test_imgs/actual_3.png",
+            "test_imgs/expected_3.png",
+        ),
+    ],
+)
+def test_image_generator(image_generator, input_file, actual_file, expected_file):
+    input_path = abs_path(input_file)
+    actual_path = abs_path(actual_file)
+    expected_path = abs_path(expected_file)
+    image_generator(input_path, actual_path)
+    assert files_equal(actual_path, expected_path)
